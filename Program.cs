1200bf0361983bc9163ad60afa68cc4cc138dbda
yasin <yasinsivis9@gmail.com>
2022-08-16 11:25:21 +0300

    dosyalar yüklendi

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int mevcut=0;
+            Console.Write("Kaç Eleman Girmek İstiyorsunuz: ");
+            mevcut = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            string[] Person = new string[mevcut];
+ 
+            for(int i=0;i<mevcut;i++)
+            {
+                Console.Write((i+1)+"."+"Eleman Adını Giriniz : ");
+                Person[i]=Console.ReadLine();
+           
+                
+            }
+            Array.Reverse(Person);
+            foreach (var pers in Person)
+            {
+                Console.WriteLine(pers);
+            }
+       
+        
+ 
+         
